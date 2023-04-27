286d878e70f75761330b897400d1dc1b29d17c6f
Diego Romero <irommero@protonmail.com>
2023-04-26 18:18:21 -0600

    Agregando entidad guardia comentada

--- src/models/entities/Guardia.ts
+++ src/models/entities/Guardia.ts
@@ -1,95 +1,95 @@
-import { Entity, PrimaryGeneratedColumn, Column, Repository, QueryFailedError } from 'typeorm';
-import DatabaseConnection from '../../database/DatabaseConnection';
+// import { Entity, PrimaryGeneratedColumn, Column, Repository, QueryFailedError, DataSource } from 'typeorm';
+// import DatabaseConnection from '../../database/DatabaseConnection';
 
-@Entity({ name: 'guardias' })
-export default class Guardia {
-    @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
-    public id: number;
+// @Entity({ name: 'guardias' })
+// export default class Guardia {
+//     @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
+//     public id: number;
 
-    @Column({ type: 'varchar', length: 20, nullable: false, unique: true })
-    public nombre: string;
+//     @Column({ type: 'varchar', length: 20, nullable: false, unique: true })
+//     public nombre: string;
 
-    @Column({ type: 'varchar', length: 32, nullable: false })
-    public turno: string;
+//     @Column({ type: 'varchar', length: 32, nullable: false })
+//     public turno: string;
 
-    @Column({ type: 'datetime', nullable: false })
-    public fechaCreacion: Date;
+//     @Column({ type: 'datetime', nullable: false })
+//     public fechaCreacion: Date;
 
-    @Column({ type: 'datetime', nullable: false })
-    public fechaActualizacion: Date;
+//     @Column({ type: 'datetime', nullable: false })
+//     public fechaActualizacion: Date;
 
-    private constructor(
-        id: number | undefined,
-        nombre: string,
-        turno: string,
-        fechaCreacion: Date,
-        fechaActualizacion: Date
-    ) {
-        this.id = <number>id;
-        this.nombre = nombre;
-        this.turno = turno;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaActualizacion = fechaActualizacion;
-    }
+//     private constructor(
+//         id: number | undefined,
+//         nombre: string,
+//         turno: string,
+//         fechaCreacion: Date,
+//         fechaActualizacion: Date
+//     ) {
+//         this.id = <number>id;
+//         this.nombre = nombre;
+//         this.turno = turno;
+//         this.fechaCreacion = fechaCreacion;
+//         this.fechaActualizacion = fechaActualizacion;
+//     }
 
-    public static async registrar(
-        nombreGuardia: string,
-        turno: string,
-    ): Promise<Guardia> {
-        const repositorioGuardias = await this.obtenerRepositorioGuardias();
+//     public static async registrar(
+//         nombreGuardia: string,
+//         turno: string,
+//     ): Promise<Guardia> {
+//         const repositorioGuardias = await this.obtenerRepositorioGuardias();
 
-        const fechaCreacion = new Date();
+//         const fechaCreacion = new Date();
 
-        const guardia = new Guardia(
-            undefined,
-            nombreGuardia,
-            turno,
-            fechaCreacion,
-            fechaCreacion
-        );
+//         const guardia = new Guardia(
+//             undefined,
+//             nombreGuardia,
+//             turno,
+//             fechaCreacion,
+//             fechaCreacion
+//         );
 
-        try {
-            await repositorioGuardias.save(guardia);
-        } catch (e) {
-            if (e instanceof QueryFailedError && e.message.includes('ER_DUP_ENTRY')) {
-                throw new Error('ErrorNombreGuardiaDuplicado');
-            }
+//         try {
+//             await repositorioGuardias.save(guardia);
+//         } catch (e) {
+//             if (e instanceof QueryFailedError && e.message.includes('ER_DUP_ENTRY')) {
+//                 throw new Error('ErrorNombreGuardiaDuplicado');
+//             }
 
-            throw e;
-        }
+//             throw e;
+//         }
 
-        return guardia;
-    }
+//         return guardia;
+//     }
 
-    public static async buscarPorNombreUsuarioYPassword(
-        nombreGuardia: string,
-        turno: string
-    ): Promise<Guardia> {
-        const repositorioGuardias = await this.obtenerRepositorioGuardias();
+//     public static async buscarPorNombreUsuarioYPassword(
+//         nombre: string,
+//         turno: string
+//     ): Promise<Guardia> {
+//         const repositorioGuardias = await this.obtenerRepositorioGuardias();
 
-        const guardia = await repositorioGuardias.findOneBy({ nombre, turno });
+//         const guardia = await repositorioGuardias.findOneBy({ nombre, turno });
 
-        if (!guardia) {
-            throw new Error('ErrorGuardiaNoEncontrado');
-        }
+//         if (!guardia) {
+//             throw new Error('ErrorGuardiaNoEncontrado');
+//         }
 
-        return guardia;
-    }
+//         return guardia;
+//     }
 
-    public static async buscarPorId(id: number): Promise<Guardia> {
-        const repositorioGuardia = await this.obtenerRepositorioGuardias();
+//     public static async buscarPorId(id: number): Promise<Guardia> {
+//         const repositorioGuardia = await this.obtenerRepositorioGuardias();
 
-        const guardia = await repositorioGuardia.findOneBy({ id });
+//         const guardia = await repositorioGuardia.findOneBy({ id });
 
-        if (!guardia) {
-            throw new Error('ErrorGuardiaNoEncontrado');
-        }
+//         if (!guardia) {
+//             throw new Error('ErrorGuardiaNoEncontrado');
+//         }
 
-        return guardia;
-    }
+//         return guardia;
+//     }
 
-    private static async obtenerRepositorioGuardias(): Promise<Repository<Guardia>> {
-        const databaseConnection = await DatabaseConnection.getConnectedInstance();
-        return databaseConnection.getRepository(Guardia);
-    }
-}
+//     private static async obtenerRepositorioGuardias(): Promise<Repository<Guardia>> {
+//         const databaseConnection = await DatabaseConnection.getConnectedInstance();
+//         return databaseConnection.getRepository(Guardia);
+//     }
+// }
