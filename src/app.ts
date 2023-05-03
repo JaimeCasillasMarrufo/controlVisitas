fedd4e56a599897882ca9b39bc5b34f24aac0697
Emilio Gonzalez <guimelemilio@gmail.com>
2023-05-03 15:11:33 -0600

    Agregando modificaciones a app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express,{ Application}  from "express";
 import bodyParser from "body-parser";
 import cors, { CorsOptions } from 'cors';
+import GuardiasController from "./controllers/GuardiasController";
 
 const app:Application=express();
 const corsOptions:CorsOptions={
-    origin:"http://localhost:3000",
+    origin:"http://localhost:8080",
     optionsSuccessStatus:200
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+
+
+GuardiasController.mount(app);
 
 export default app;
